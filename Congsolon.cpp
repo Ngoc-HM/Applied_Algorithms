1b87c0cf631aa534500fa2fd5ec6453c8b9531df
Hoàng Minh Ngọc <91311915+NgocJQK@users.noreply.github.com>
2022-12-24 00:29:44 +0700

    Cong So Lon
    Cong So Lon

---
+++ Congsolon.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+string Add(string a, string b)
+{
+    string res = "";
+    int nho = 0;
+    while (a.size() < b.size())
+        a = "0" + a;
+    while (a.size() > b.size())
+        b = "0" + b;
+    int len = a.size() - 1;
+    for (int i = len; i >= 0; i--)
+    {
+        int so = int(a[i] - '0') + int(b[i] - '0') + nho;
+        res = char(so % 10 + '0') + res;
+        nho = so / 10;
+    }
+    if (nho > 0)
+        res = char(nho + '0') + res;
+    return res;
+}
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    cout << Add(a, b) << endl;
+    return 0;
+}
